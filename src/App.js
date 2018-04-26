66846a3d88d1fac1cae64106bf514a42e8a45318
Lucas Scariot <lucas@scariot.fr>
2018-04-26 21:52:46 +0200

    improve design

--- src/App.js
+++ src/App.js
@@ -1,96 +1,181 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import _ from 'lodash';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import { RadarChart, PolarGrid, PolarAngleAxis, Radar } from 'recharts'
+import { Row, Col, List, Tabs, Layout, Form, Input, Button } from 'antd'
+import _ from 'lodash'
+import 'antd/dist/antd.css'
 
-const GITHUB_KEY = '?access_token=027c1d26e0734fe194f9f1c78db04ba4c1b2d020';
+import './App.css'
+
+const { Header, Content } = Layout
+const { TabPane } = Tabs
+
+const GITHUB_KEY = '?access_token=027c1d26e0734fe194f9f1c78db04ba4c1b2d020'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      username: 'lucasscariot',
       repos: [],
       skills: [],
-      user: {},
+      activity: [],
+      currentTab: '1',
     }
   }
 
-  getUser = async (username) => {
-    const { data } = await axios(`https://api.github.com/users/${username}/repos${GITHUB_KEY}`);
+  getUser = async () => {
+    const { username } = this.state
+    const { data } = await axios(`https://api.github.com/users/${username}/repos${GITHUB_KEY}`)
     this.setState({ repos: data.filter(repo => !repo.fork) })
   }
 
   getReposLanguages = async (url) => {
-    const { data } = await axios(url + GITHUB_KEY);
-    return data;
+    const { data } = await axios(url + GITHUB_KEY)
+    return data
   }
 
   skillsMap = (skills) => {
-    const { size } = skills.reduce((a, b) => ({ size: a.size + b.size }));
+    const { size } = skills.reduce((a, b) => ({ size: a.size + b.size }))
 
     return _.sortBy(skills.map(skill => ({
       ...skill,
       pourcent: _.round((100 * skill.size) / size, 1),
-    })), ['size']).reverse();
+    })), ['size']).reverse()
   }
 
   getSkills = async () => {
     const { repos } = this.state
-    let mappedLanguages = [];
+    const mappedLanguages = []
 
     const reposLanguages = await Promise.all(repos.map(async ({ languages_url, name }) => ({
       languages: await this.getReposLanguages(languages_url),
       data: { repo: name },
-    })));
+    })))
 
     reposLanguages.forEach(({ languages, data: { repo } }) => {
-      const keys = Object.keys(languages);
+      const keys = Object.keys(languages)
 
-      if (keys.length === 0) return;
+      if (keys.length === 0) return
 
       keys.forEach((name) => {
-        const index = mappedLanguages.findIndex(l => l.name === name);
+        const index = mappedLanguages.findIndex(l => l.name === name)
         if (index === -1) {
-          mappedLanguages.push({ name, size: languages[name], repos: [repo] });
+          mappedLanguages.push({ name, size: languages[name], repos: [repo] })
         } else {
-          mappedLanguages[index].repos.push(repo);
-          mappedLanguages[index].size += languages[name];
+          mappedLanguages[index].repos.push(repo)
+          mappedLanguages[index].size += languages[name]
         }
-      });
-    });
+      })
+    })
 
+    if (mappedLanguages.length === 0) return
 
-    this.setState({ skills: this.skillsMap(mappedLanguages) })
+    this.setState({ skills: this.skillsMap(mappedLanguages).filter(l => l.pourcent > 1) })
+  }
 
-
+  getActivity = async () => {
+    const { username } = this.state
+    const { data } = await axios(`https://api.github.com/users/${username}/events${GITHUB_KEY}`)
+    this.setState({ activity: data })
   }
 
   async componentDidMount() {
-    await this.getUser('lucasscariot')
-    await this.getSkills()
+    await this.fetchData()
   }
 
+  fetchData = async () => {
+    await this.getUser()
+    await this.getSkills()
+    await this.getActivity()
+  }
+
+  renderEvent = (event) => {
+    const { type } = event
+
+    switch (type) {
+      case 'PushEvent':
+        return `Push [${event.repo.name}] - ${event.payload.commits[0].message.slice(0, 50)}`
+      case 'WatchEvent':
+        return `${event.payload.action} - ${event.repo.name}`
+      default:
+        return type
+    }
+  }
+
+  onChange = e => this.setState({ username: e.target.value })
+
   render() {
-    const { repos, skills } = this.state
+    const {
+      repos, skills, activity, username, currentTab,
+    } = this.state
 
     return (
-      <div className="App">
-        <h2>Repos</h2>
-        <ul>
-          {repos.map((repo) => (
-            <li key={repo.name}>{repo.name}</li>
-          ))}
-        </ul>
-        <h2>Skills</h2>
-        <ul>
-          {skills.map((skill) => (
-            <li key={skill.name}>{skill.name} | {skill.pourcent}%</li>
-          ))}
-        </ul>
-      </div>
-    );
+      <Layout>
+        <Header style={{ color: 'white' }}>
+          User: {username}
+        </Header>
+        <Row gutter={48} style={{ background: '#fff' }}>
+          <Col span={12}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Form layout="inline">
+                <Form.Item>
+                  <Input placeholder="Username" value={username} onChange={this.onChange} />
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit" onClick={this.fetchData}>Search</Button>
+                </Form.Item>
+              </Form>
+            </div>
+            <RadarChart
+              cx={300}
+              cy={250}
+              outerRadius={150}
+              width={600}
+              height={500}
+              data={skills}
+            >
+              <PolarGrid />
+              <PolarAngleAxis dataKey="name" />
+              <Radar name="name" dataKey="size" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            </RadarChart>
+          </Col>
+          <Col span={10}>
+            <Tabs
+              defaultActiveKey={currentTab}
+              onChange={e => this.setState({ currentTab: e })}
+            >
+              <TabPane tab={`Repositories (${repos.length})`} key="1">
+                <div style={{ margin: 20 }}>
+                  <List
+                    itemLayout="horizontal"
+                    bordered
+                    dataSource={repos.map(s => s.name)}
+                    renderItem={item => (
+                      <List.Item>{item}</List.Item>
+                    )}
+                  />
+                </div>
+              </TabPane>
+              <TabPane tab="Activity" key="2">
+                <div style={{ margin: 20 }}>
+                  <List
+                    itemLayout="horizontal"
+                    bordered
+                    dataSource={activity.map(this.renderEvent)}
+                    renderItem={item => (
+                      <List.Item>{item}</List.Item>
+                    )}
+                  />
+                </div>
+              </TabPane>
+            </Tabs>
+          </Col>
+        </Row>
+      </Layout>
+    )
   }
 }
 
-export default App;
+export default App
